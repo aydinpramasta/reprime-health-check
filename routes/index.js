143354159b1b3ba4284a71b7b1cc215f25216c02
Aydin Pramasta <aydin.pram@gmail.com>
2023-05-11 13:26:11 +0700

    install packages + reformat

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Not Implemented.');
+router.get("/", (req, res, next) => {
+  res.send("Not Implemented.");
 });
 
 module.exports = router;
