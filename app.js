9f69ad96b094727a58f6d876fad5cc05d8097ea2
Aydin Pramasta <aydin.pram@gmail.com>
2023-05-14 19:00:16 +0700

    rewrite codebase to use telegraf + polling architecture

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-require("dotenv").config();
+import cron from "node-cron";
+import bot from "./lib/telegraf.js";
+import { startCommandHandler, statusCommandHandler } from "./lib/commands.js";
+import { healthCheckCronHandler } from "./lib/crons.js";
 
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cron = require("node-cron");
-const {
-  initTelegramWebhook,
-  telegramWebhookHandler,
-  telegramHealthCheck,
-} = require("./lib/telegram");
+cron.schedule("*/2 * * * *", healthCheckCronHandler);
 
-const app = express();
+bot.start(startCommandHandler);
+bot.command("status", statusCommandHandler);
 
-// Run health check every 2 minutes
-cron.schedule("*/2 * * * *", async () => {
-  await telegramHealthCheck(
-    "https://api.app.reprime.id/api/v2/webs/list-article"
-  );
-});
+await bot.launch();
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-const telegramWebhookUrl = `/webhook/${process.env.TELEGRAM_BOT_TOKEN}`;
-initTelegramWebhook(telegramWebhookUrl);
-app.post(telegramWebhookUrl, telegramWebhookHandler);
-
-module.exports = app;
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
