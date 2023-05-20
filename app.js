efe656675dfd9f554c278bb49bcfaa7ed38a0695
Aydin Pramasta <aydin.pram@gmail.com>
2023-05-20 21:45:37 +0700

    fix cron timezone

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import cron from "node-cron";
 import bot from "./lib/telegraf.js";
-import startCommandHandler from "./commands/startCommand.js";
-import statusCommandHandler from "./commands/statusCommand.js";
 import healthCheckPerTwoMinutesCronHandler from "./crons/healthCheckPerTwoMinutes.js";
 import healthCheckPerOneHourCronHandler from "./crons/healthCheckPerOneHour.js";
+import startCommandHandler from "./commands/startCommand.js";
+import statusCommandHandler from "./commands/statusCommand.js";
 import subscribeCommandHandler from "./commands/subscribeCommand.js";
 import unsubscribeCommandHandler from "./commands/unsubscribeCommand.js";
 
-cron.schedule("*/2 * * * *", healthCheckPerTwoMinutesCronHandler); // every 2 minutes
-cron.schedule("0 6-18 * * *", healthCheckPerOneHourCronHandler); // every hour from 06.00 to 18.00
+const cronOptions = { timezone: "Asia/Jakarta" };
+
+cron.schedule("*/2 * * * *", healthCheckPerTwoMinutesCronHandler, cronOptions); // every 2 minutes
+cron.schedule("0 6-18 * * *", healthCheckPerOneHourCronHandler, cronOptions); // every hour from 06.00 to 18.00
 
 bot.start(startCommandHandler);
 bot.command("status", statusCommandHandler);
